027d73ac0dc8998ae307286d1472627d21475154
Gabrie345 <77858768+Gabrie345@users.noreply.github.com>
2022-03-09 15:52:51 -0300

    first commit

---
+++ src/test/java/br/ce/wcaquino/entidades/LocacaoTest.java
@@ -0,0 +1,14 @@
+package br.ce.wcaquino.entidades;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LocacaoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
